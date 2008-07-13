b42aae2706ae1ac7476bb3b1f16fa9fdc630098d
Felipe Diesel <felipediesel@gmail.com>
2008-07-13 04:01:09 -0300

    Instalação do

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  $(".flash_notice, .flash_error, #errorExplanation").click(function(){
+    $(this).slideUp("fast");
+  });  
+})
