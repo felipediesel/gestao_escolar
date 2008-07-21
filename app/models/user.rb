29b2a5f79d315a2862b8ceb326c3eaa93b05a300
Felipe Diesel <felipediesel@gmail.com>
2008-07-20 21:14:12 -0300

    Correção para verificação e validação da senha

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
+  before_save :crypt_password
+  
   validates_presence_of :password, :if => :password_required?
   validates_presence_of :password_confirmation, :if => :password_required?
   validates_confirmation_of :password, :if => :password_required?
   validates_length_of :password, :within => 6..40, :if => :password_required?
   
-  def password=(p)
-    self[:password] = Digest::SHA1.hexdigest(p)  unless p.blank?
+  def crypt_password
+    self[:password] = Digest::SHA1.hexdigest(password) unless password.blank?
   end   
   
   def password_required?
-    new_record? || password.blank?
+    new_record? || !password.blank?
   end
 end
